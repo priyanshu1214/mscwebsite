e3b401c0d25baed74c26404cf24ad1256ed7ded2
Peeyu1214 <98081268+Peeyu1214@users.noreply.github.com>
2024-05-10 16:43:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+function func1()
+{
+   document.getElementById("dgdp").innerHTML="flamingo"
+}
+
+function func2()
+{
+    
+    document.getElementById("dgdp").innerHTML="humming bird"
+}
+
+function func3()
+{
+    document.getElementById("dgdp").innerHTML="macaw parrot"
+}
+
+function func4()
+{
+    document.getElementById("dgdp").innerHTML="bald eagle"
+}
+function func5()
+{
+    document.getElementById("dgdp").innerHTML="arctic bird"
+}
