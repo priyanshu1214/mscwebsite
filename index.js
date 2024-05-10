2b9471688345262506e8e1287f3e2226a298043e
Peeyu1214 <98081268+Peeyu1214@users.noreply.github.com>
2024-05-10 17:34:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 function func1()
 {
-   document.getElementById("dgdp").innerHTML="flamingo"
+   document.getElementById("dgdp").innerHTML="<h1>flamingo</h1>"
 }
 
 function func2()
 {
     
-    document.getElementById("dgdp").innerHTML="humming bird"
+    document.getElementById("dgdp").innerHTML="<h1>humming bird</h1>"
 }
 
 function func3()
 {
-    document.getElementById("dgdp").innerHTML="macaw parrot"
+    document.getElementById("dgdp").innerHTML="<h1>macaw parrot</h1>"
 }
 
 function func4()
 {
-    document.getElementById("dgdp").innerHTML="bald eagle"
+    document.getElementById("dgdp").innerHTML="<h1>bald eagle</h1>"
 }
 function func5()
 {
-    document.getElementById("dgdp").innerHTML="arctic bird"
+    document.getElementById("dgdp").innerHTML="<h1>arctic bird</h1>"
 }
